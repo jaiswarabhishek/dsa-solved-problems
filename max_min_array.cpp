ac56d873d1f0e9d6a29a8f5818dad61a3186b264
Abhishek <jangal&mangal.gov>
2023-01-08 14:41:08 +0530

    Initial Commit

---
+++ max_min_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+//Input: arr[] = {3, 5, 4, 1, 9}
+//Output: Minimum element is: 1
+//        Maximum element is: 9
+
+
+
+    return 0;
+}
